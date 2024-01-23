79f3c320c6663666e3c3770dd0abae397a479013
Alina Nosovets <alinanosovets6849@gmail.com>
2024-01-23 19:49:38 +0700

    feat: rename.js completed

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,26 @@
+import fsPromises from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const rename = async () => {
-    // Write your code here 
+    const filesDir = path.join(__dirname, 'files');
+
+    try {
+        await fsPromises.access(path.join(filesDir, 'properFilename.md'));
+        throw new Error('FS operation failed');
+    } catch(err) {
+        if (err.code === 'ENOENT') {
+            const fileToRename = path.join(filesDir, 'wrongFilename.txt');
+            const fileToRenameTo = path.join(filesDir, 'properFilename.md');
+            await fsPromises.rename(fileToRename, fileToRenameTo);
+            console.log('File renamed');
+        } else {
+            throw err;
+        }
+    }
 };
 
 await rename();
