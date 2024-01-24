218a8d0eb82abe9b7c67c828402644a3341d281f
Alina Nosovets <alinanosovets6849@gmail.com>
2024-01-24 17:09:17 +0700

    feat: env.js is competed

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,9 @@
 const parseEnv = () => {
-    // Write your code here 
+    const envVariables = Object.entries(process.env)
+        .filter(entry => entry[0].includes('RSS_'))
+        .map(variable => `${variable[0]}=${variable[1]}`)
+        .join('; ');
+    console.log(envVariables);
 };
 
 parseEnv();
