24af6fc4e9e38aa636ddd5e5371e41d5633490ed
Alina Nosovets <alinanosovets6849@gmail.com>
2024-01-24 14:31:14 +0700

    feat: delete.js completed

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import fsPromises from 'fs/promises';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+    const fileToRemove = path.join(__dirname, 'files/fileToRemove.txt');
+    try {
+        await fsPromises.access(fileToRemove);
+        await fsPromises.rm(fileToRemove);
+        console.log('File removed');
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            throw new Error('FS operation failed');
+        } else {
+            throw err;
+        }
+    }
 };
 
 await remove();
