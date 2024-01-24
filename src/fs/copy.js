c8a74117903948071c5c2b3a144c23fdc13782ca
Alina Nosovets <alinanosovets6849@gmail.com>
2024-01-24 15:06:27 +0700

    fix: fix copy.js implementation

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -6,19 +6,22 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const copy = async () => {
-    const finalFolder = 'files_copy';
+    const copyFolderName = 'files_copy';
+    const copyFolderDir = path.join(__dirname, copyFolderName);
+    
     try {
-        await fsPromises.access(finalFolder);
+        await fsPromises.access(copyFolderDir);
         throw new Error('FS operation failed');
+
     } catch (err) {
         if (err.code === 'ENOENT') {
-            await fsPromises.mkdir(finalFolder, { recursive: true });
+            await fsPromises.mkdir(copyFolderDir, { recursive: true });
             const sourceDir = path.join(__dirname, 'files');
             const sourceFiles = await fsPromises.readdir(sourceDir);
 
             for (const file of sourceFiles) {
                 const sourceFile = path.join(sourceDir, file);
-                const finalFile = path.join(finalFolder, file);
+                const finalFile = path.join(copyFolderDir, file);
 
                 await fsPromises.copyFile(sourceFile, finalFile);
             }
