cfc65e796c5e99240d7c7d5b0451d78f942e5bdc
Alina Nosovets <alinanosovets6849@gmail.com>
2024-01-25 20:15:48 +0700

    feat: decompress.js is completed

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,38 @@
+import path from 'path';
+import fs from 'fs';
+import zlib from 'zlib';
+import fsPromises from 'fs/promises';
+import { pipeline } from 'stream/promises';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const decompress = async () => {
-    // Write your code here 
+    const sourceFile = path.join(__dirname, 'files/archive.gz');
+    const targetFile = path.join(__dirname, 'files/fileToCompress.txt');
+
+    try {
+        await fsPromises.access(sourceFile);
+        const readibleStream = fs.createReadStream(sourceFile);
+        const writebleStream = fs.createWriteStream(targetFile);
+
+        const gunZip = zlib.createGunzip();
+
+        await pipeline(
+            readibleStream,
+            gunZip,
+            writebleStream
+        )
+        fs.unlinkSync(sourceFile);
+        console.log('Decompress is successful!');
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            throw new Error('Please compress the file first!')
+        } else {
+            throw err;
+        }
+    }
 };
 
 await decompress();
