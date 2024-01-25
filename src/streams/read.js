bdd1e0977cd4c26d1ca13c083130387fc4c40530
Alina Nosovets <alinanosovets6849@gmail.com>
2024-01-25 16:12:19 +0700

    feat: streams/read.js is completed

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const fileToRead = path.join(__dirname, 'files/fileToRead.txt');
+    const readableStream = fs.createReadStream(fileToRead);
+
+    readableStream.on('data', (chunk) => {
+        process.stdout.write(chunk)
+    });
 };
 
 await read();
