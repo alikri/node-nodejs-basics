faa9d1729753c0ce81d0a5d4292b5bf3628ceaaa
Alina Nosovets <alinanosovets6849@gmail.com>
2024-01-25 16:31:28 +0700

    fest: streams/write is completed

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const targetFile = path.join(__dirname, 'files/fileToWrite.txt');
+    const writableStream = fs.createWriteStream(targetFile);
+    
+    process.stdin.pipe(writableStream);
+
 };
 
 await write();
