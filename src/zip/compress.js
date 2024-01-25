e046df57119ebccf468bc7391bd9cd35db6552f2
Alina Nosovets <alinanosovets6849@gmail.com>
2024-01-25 19:50:53 +0700

    feat: compress.js is completed

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,29 @@
+import path from 'path';
+import fs from 'fs';
+import zlib from 'zlib';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    const finalFile = path.join(__dirname, 'files/archive.gz');
+    const sourceFile = path.join(__dirname, 'files/fileToCompress.txt');
+
+    const readableStream = fs.createReadStream(sourceFile);
+    const writableStream = fs.createWriteStream(finalFile);
+
+    const gZip = zlib.createGzip();
+    readableStream.pipe(gZip).pipe(writableStream);
+
+    writableStream.on('finish', () => {
+    console.log('All went well, file compressed')
+    });
+
+    writableStream.on('error', (err) => {
+        console.log('Something went wrong:', err)
+    })
+
 };
 
 await compress();
