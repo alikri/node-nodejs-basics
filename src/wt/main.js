7bb96245ff507f08864e899fc0f2bb8c87b5b976
Alina Nosovets <alinanosovets6849@gmail.com>
2024-01-26 13:40:59 +0700

    feat: main.js complete

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,44 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const coresQuantity = os.cpus().length;
+    const workersResults = [];
+    const defaultNumber = 10;
+
+    for (let i = defaultNumber; i < coresQuantity; i++) {
+
+        const workerData = await new Promise((resolve, reject) => {
+            const worker = new Worker('./worker.js', {
+                workerData: i,
+            });
+            
+            worker.on('message', (data) => {
+                resolve({ status: 'resolved', data });
+            })
+
+            worker.on('error', () => {
+                reject({ status: 'error', data: null });
+            })
+
+        })
+
+        workersResults.push(workerData);
+    
+    }
+
+    Promise.allSettled(workersResults)
+        .then(results => {
+            const processedResults = results.map(result => result.status === 'fulfilled' ?
+                { status: 'resolved', data: result.value.data } :
+                { status: 'error', data: null }
+            );
+            
+            console.log(processedResults);
+        }).catch(err => {
+            console.error('Something went wrong...', err);
+        }) 
+
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,5 +1,3 @@
-// n should be received from main thread
-
 import { isMainThread, parentPort, workerData } from 'worker_threads';
 
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
