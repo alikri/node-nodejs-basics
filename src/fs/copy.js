e0e30385f829ed810e973ba225ee6e6d029e597d
Alina Nosovets <alinanosovets6849@gmail.com>
2024-01-26 15:37:20 +0700

    fix: update implementation of fs/copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -6,30 +6,46 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const copy = async () => {
-    const copyFolderName = 'files_copy';
-    const copyFolderDir = path.join(__dirname, copyFolderName);
+    const sourceFolderDir = path.join(__dirname, 'files');
+    const copyFolderDir = path.join(__dirname, 'files_copy');
     
     try {
-        await fsPromises.access(copyFolderDir);
-        throw new Error('FS operation failed');
 
-    } catch (err) {
-        if (err.code === 'ENOENT') {
-            await fsPromises.mkdir(copyFolderDir, { recursive: true });
-            const sourceDir = path.join(__dirname, 'files');
-            const sourceFiles = await fsPromises.readdir(sourceDir);
+        try {
+            await fsPromises.access(copyFolderDir);
+            throw new Error('FS operation failed'); 
 
-            for (const file of sourceFiles) {
-                const sourceFile = path.join(sourceDir, file);
-                const finalFile = path.join(copyFolderDir, file);
-
-                await fsPromises.copyFile(sourceFile, finalFile);
+        } catch (err) {
+            if (err.code !== 'ENOENT') {
+                throw err; 
+            }
+        }
+        
+        try {
+            await fsPromises.access(sourceFolderDir);
+            
+        } catch (err) {
+            if (err.code === 'ENOENT') {
+                throw new Error('FS operation failed'); 
             }
 
-            console.log("Directory copied")
-        } else {
-            throw err;
+            throw err; 
         }
+
+        await fsPromises.mkdir(copyFolderDir, { recursive: true });
+        const sourceFiles = await fsPromises.readdir(sourceFolderDir);
+
+        for (const file of sourceFiles) {
+            const sourceFile = path.join(sourceFolderDir, file);
+            const finalFile = path.join(copyFolderDir, file);
+
+            await fsPromises.copyFile(sourceFile, finalFile);
+        }
+
+        console.log('Directory copied');
+
+    } catch (err) {
+            throw err;
     }
 };
 
